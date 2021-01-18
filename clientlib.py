1fbda6b85cf98b43838f2b8f6a27607c9511210f
Abhijeet Srivastava <abhijeet181995@gmail.com>
2021-01-19 00:02:27 +0530

    P2P chat completed

--- clientlib.py
+++ clientlib.py
@@ -1,41 +1,53 @@
 import socket,pickle
+import threading
 HOST = '127.0.0.1'
-PORT = 54001
+PORT = 54005
 
 class client:
-    def __init__(self,name,rollnum,pswd,port):
-            self.name = name
-            self.rollnum = rollnum
-            self.pswd=pswd
-            self.online = 0
-            self.choice=-1
+    def __init__(self,port):
             self.port = port
-    def signin(self):
+            self.online = 0
+    def signin(self,name,pswd):
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((HOST,PORT))
-        self.online = 1
-        self.choice = 1
-        s.send(pickle.dumps(self))
-        print(s.recv(1).decode())
+        cpy = self
+        cpy.choice = 1
+        cpy.pswd = pswd
+        cpy.name = name
+        s.send(pickle.dumps(cpy))
+        if s.recv(1).decode()=="1":
+            self.online = 1
+
+            return 1
+        else:
+            return 0
         s.close()
-    def signup(self):
+    def signup(self,name,rollnum,pswd):
+        self.name = name
+        self.rollnum = rollnum
+        self.pswd = pswd
+        self.choice = -1
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((HOST,PORT))
         self.choice = 2
         s.sendall(pickle.dumps(self))
-        print(s.recv(1).decode())
+        if s.recv(1).decode()=="1":
+            threading.Thread(target=self.listen).start()
+            return 1
+        else:
+            return 0
         s.close()
-    def server(self):
+    def listen(self):
         host = '127.0.0.1'
         server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
-        print(self.port)
         server.bind((host,self.port))
         server.listen()
         while True:
             conn,addr=server.accept()
             print(conn.recv(1024).decode())
             print(addr)
+            conn.close()
     def message(self,name,msg):
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((HOST,PORT))
--- server.py
+++ server.py
@@ -1,33 +1,3 @@
-import clientlib
-import socket,pickle
+import serverlib
 
-HOST = '127.0.0.1'
-PORT = 54001
-
-clientlist = {}
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
-server.bind((HOST,PORT))
-server.listen()
-
-while True:
-    conn,addr=server.accept()
-    data = pickle.loads(conn.recv(2048))
-    if data.choice==1:
-        if data.name in clientlist.keys():
-            clientlist[data.name].online=1
-            conn.send(b"1")
-        else:
-            conn.send(b"0")
-        
-    if data.choice==2:
-        clientlist[data.name]=data
-        print(data.name)
-        conn.send(b"1")
-    if data.choice==3:
-        data.port = clientlist[data.name].port
-        conn.sendall(pickle.dumps(data))
-
-    conn.close()
-
-server.close()
+s = serverlib.server()
